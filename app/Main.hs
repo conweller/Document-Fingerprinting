94db574ccc175eb65fa6d3dc95b16bf2ab7458d5
Connor Onweller <onweller@udel.edu>
2022-03-27 18:13:20 -0400

    Added locational information and documentation for fingerprints

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
+import Fingerprint
 
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  contents <- readFile "../../abstract/plagiarism-dataset/src/A2016/Z1/Z1/student1013.c"
+  print $ fingerprint 4 10 contents
--- src/Fingerprint.hs
+++ src/Fingerprint.hs
@@ -8,25 +8,43 @@ import           Control.Monad.Trans.State      ( evalState
 import           Data.Char                      ( isAlphaNum
                                                 , toLower
                                                 )
+import           Data.Foldable                  ( minimumBy )
 import           Data.List.Split                ( chunksOf )
 import           Data.Tuple.Extra               ( dupe )
 import qualified Token
 import           Token                          ( Token )
 
+-- | /O(n)/ 'getTokens' @s@ returns the list of tokens for the relevant characters in
+-- @s@
 getTokens :: String -> [Token]
 getTokens = map (Token.fromChar . toLower) . filter isAlphaNum
 
-hashes :: Int -> String -> [Int]
-hashes k s = (evalState . traverse state)
-  (zipWith hashWithPrev (0 : tokenHashes) (firstHash : drop k tokenHashes))
-  0
+-- | /O(n)/ 'rollingHash' @k@ @s@ returns a list of hashes of the @k@-grams in
+-- @s@ using the rolling hashing function described in Karp and Rabin
+-- https://doi.org/10.1147/rd.312.0249
+rollingHash :: Int -> String -> [Int]
+rollingHash k s = evalState hashStates 0
  where
   tokenHashes = map Token.ord (getTokens s)
   base        = Token.ord (maxBound :: Token)
   bases       = map (base ^) [k - 1, k - 2 .. 0]
   hashWithPrev prevTokenHash newTokenHash prevHash =
     dupe $ (prevHash - prevTokenHash * base ^ (k - 1)) * base + newTokenHash
-  firstHash = sum (zipWith (*) bases (take k tokenHashes))
+  firstHash  = sum (zipWith (*) bases (take k tokenHashes))
+  hashStates = traverse
+    state
+    (zipWith hashWithPrev (0 : tokenHashes) (firstHash : drop k tokenHashes))
 
+-- | /O(w * n)/ 'fingerprint' @w@ @k@ @s@ returns the document fingerprint of @s@
+-- using the winnowing algorithm described in Schleimer, Wilkerson, and Aiken
+-- https://doi.org/10.1145/872757.872770, using windows of size @w@ on the
+-- @k@-grams of @s@
 fingerprint :: Int -> Int -> String -> [(Int, Int)]
-fingerprint w k = map minimum . chunksOf w . (`zip` [0 ..]) . hashes k
+fingerprint w k =
+  map
+      (minimumBy
+        (\pair1 pair2 -> (negate <$> pair1) `compare` (negate <$> pair2))
+      )
+    . chunksOf w
+    . (`zip` [0 ..])
+    . rollingHash k

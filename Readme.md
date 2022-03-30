1e027b19cce6f786b017cad3f700199a52a30e6a
Connor Onweller <onweller@udel.edu>
2022-03-30 00:46:01 -0400

    Avoid hard-coded directory, add readme instruction

--- Readme.md
+++ Readme.md
@@ -1,2 +1,10 @@
-Plagiarism Detection via Document Fingerprinting using [this
+# Source Code Plagiarism Detector
+
+Source code plagiarism detection via document fingerprinting using [this
 algorithm](https://doi.org/10.1145/872757.872770)
+
+Run with:
+
+``` sh
+cabal run /path/to/src/files/
+```
--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,16 @@ module Main
   ( main
   ) where
 
-import           Fingerprint
-import           Query
-import           System.Directory
+import           Fingerprint                    ( fingerprint )
+import           Query                          ( allToAll )
+import           System.Directory               ( listDirectory )
+import           System.Environment             ( getArgs )
 import qualified System.IO.Strict              as S
 
 
-dir :: [Char]
-dir = "../../abstract/plagiarism-dataset/src/A2016/Z1/Z1/"
-
 main :: IO ()
 main = do
+  dir          <- head <$> getArgs
   fingerprints <- listDirectory dir
     >>= mapM (\path -> fingerprint path w k <$> S.readFile (dir ++ path))
   print $ allToAll t fingerprints
